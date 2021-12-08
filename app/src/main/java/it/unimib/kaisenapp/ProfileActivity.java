210dc6c1c59923df9ce555ea4784a7c0d3992331
MatteoCovelli <m.covelli7@campus.unimib.it>
2021-12-08 17:17:47 +0100

    Aggiornamento creazione Login-Registrazione #03
    Implementata la verifica dell'email per effettuare il login.
    Implementato il logout.
    Scritto codice per visualizzare i dati dell'utente (non ancora funzionante).
    Creata classe ForgotPassword, con relativo xml, per effettuare il recupero della password.

--- app/src/main/java/it/unimib/kaisenapp/ProfileActivity.java
+++ app/src/main/java/it/unimib/kaisenapp/ProfileActivity.java
@@ -1,14 +1,83 @@
 package it.unimib.kaisenapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;  //id di firebase legato all'utente
+
+
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, LoginUser.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView greetingTextView = (TextView) findViewById(R.id.txtBenvenuto);
+        final TextView txtFullName = (TextView) findViewById(R.id.txtFullNameActivity);
+        final TextView txtMail = (TextView) findViewById(R.id.txtMailActivity);
+        final TextView txtAge = (TextView) findViewById(R.id.txtAgeActivity);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.mail;
+                    String age = userProfile.age;
+
+                    greetingTextView.setText("Welcome, " + fullName + "!");
+                    txtFullName.setText(fullName);
+                    txtMail.setText(email);
+                    txtAge.setText(age);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(ProfileActivity.this, "Something wrong happened!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 }
