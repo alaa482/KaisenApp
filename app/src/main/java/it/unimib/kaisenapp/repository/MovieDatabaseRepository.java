5bb95504a510384e1e03a724fe61a24dd548efa4
Blank <38037483+404ProgramNotFound@users.noreply.github.com>
2022-02-18 16:04:21 +0100

    episode completa veramente adesso

--- app/src/main/java/it/unimib/kaisenapp/repository/MovieDatabaseRepository.java
+++ app/src/main/java/it/unimib/kaisenapp/repository/MovieDatabaseRepository.java
@@ -18,28 +18,22 @@ import it.unimib.kaisenapp.request.MovieApiClient;
 public class MovieDatabaseRepository {
     private static MovieDatabaseRepository instance;
     private MovieDao movieDao;
-    private TvShowDao tvShowDao;
+
 
     private MovieDatabaseRepository(MovieDao movieDao) {
         this.movieDao=movieDao;
     }
 
-    private MovieDatabaseRepository(TvShowDao tvShowDao) {
-        this.tvShowDao=tvShowDao;
-    }
+
 
     public static MovieDatabaseRepository getInstance(MovieDao movieDao){
         if(instance == null)
+
             instance=new MovieDatabaseRepository(movieDao);
 
         return instance;
     }
-    public static MovieDatabaseRepository getInstance(TvShowDao tvShowDao){
-        if(instance == null)
-            instance=new MovieDatabaseRepository(tvShowDao);
 
-        return instance;
-    }
 
 
     public MutableLiveData<List<MovieEntity>> getAllMovies(){
@@ -50,14 +44,8 @@ public class MovieDatabaseRepository {
     public void updateMovie(MovieEntity movieEntity){
         movieDao.updateMovies(movieEntity);
     }
-    public void updateSerie(TvShowEntity tvShowEntity){
-        tvShowDao.updateTvSerie(tvShowEntity);
-    }
-    public MutableLiveData<List<TvShowEntity>> getAllSeries(){
-        MutableLiveData<List<TvShowEntity>> liveData= new MutableLiveData<>();
-        liveData.postValue(tvShowDao.getAllTvShows());
-        return liveData;
-    }
+
+
 
 
     public MutableLiveData<List<MovieEntity>> getAllMoviesByCategory(String category){
@@ -65,26 +53,16 @@ public class MovieDatabaseRepository {
         liveData.postValue(movieDao.getAllMoviesByCategory(category));
         return liveData;
     }
-    public MutableLiveData<List<TvShowEntity>> getAllTvShowsByCategory(String category){
-        MutableLiveData<List<TvShowEntity>> liveData= new MutableLiveData<>();
-        liveData.postValue(tvShowDao.getAllTvShowsByCategory(category));
-        return liveData;
-    }
 
     public void addMovie(MovieEntity movieEntity){
         movieDao.insertMovie(movieEntity);
     }
 
-    public void addTvShow(TvShowEntity tvShowEntity){
-        tvShowDao.insertTvShow(tvShowEntity);
-    }
 
     public void addAllMovies(List<MovieEntity> movieEntity){
         movieDao.insertAllMovies(movieEntity);
     }
-    public void addAllTvShows(List<TvShowEntity> tvShowEntities){
-        tvShowDao.insertAllTvShows(tvShowEntities);
-    }
+
 
     public MutableLiveData<List<MovieEntity>> getAllSavedMovies(){
         MutableLiveData<List<MovieEntity>> liveData= new MutableLiveData<>();
@@ -101,26 +79,8 @@ public class MovieDatabaseRepository {
         liveData.postValue(movieDao.getAllFavoriteMovies());
         return liveData;
     }
-    public MutableLiveData<List<TvShowEntity>> getAllFavoriteTvShows(){
-        MutableLiveData<List<TvShowEntity>> liveData= new MutableLiveData<>();
-        liveData.postValue(tvShowDao.getAllFavoriteTvShows());
-        return liveData;
-    }
-    public MutableLiveData<List<TvShowEntity>> getAllSavedTvShows(){
-        MutableLiveData<List<TvShowEntity>> liveData= new MutableLiveData<>();
-        liveData.postValue(tvShowDao.getAllSavedTvShows());
-        return liveData;
-    }
-    public MutableLiveData<List<TvShowEntity>> getAllWatchedTvShows(){
-        MutableLiveData<List<TvShowEntity>> liveData= new MutableLiveData<>();
-        liveData.postValue(tvShowDao.getAllWatchedTvShows());
-        return liveData;
-    }
 
     public void deleteAllMovies(){
-        movieDao.deleteAllMovies();
-    }
-    public void deleteAllTvShows(){
         movieDao.deleteAllMovies();
     }
 
